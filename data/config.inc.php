21859198a4234e9b2cee89796a794d8f1059485b
mingcheng <lucky@gracecode.com>
2013-01-21 18:39:00 +0800

    更新 README 文件

--- data/config.inc.php
+++ data/config.inc.php
@@ -8,6 +8,8 @@
  * @link   http://www.gracecode.com/
  */
 
+date_default_timezone_set('Asia/Shanghai');
+
 define("CONFIG_TIMEOUT", 100);
 define("CONFIG_USERAGENT", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
 define("CONFIG_DATABASE", __DIR__ . "/../assets/aqi.sqlite");
